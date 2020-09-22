5f0a1d51c482fd87354971cc5e684754b08322b2
808kevin <kn42@hawaii.edu>
2020-09-22 11:56:12 -1000

    a

---
+++ Lab6/Ex1.js
@@ -0,0 +1,11 @@
+var age =  19;
+
+var fav_num = 69;
+
+var day_of_birth = 9;
+
+var month_of_birth = 11;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
---
+++ Lab6/Ex5.js
@@ -0,0 +1,6 @@
+first_name = "Kevin";
+last_name = "Nguyen";
+
+full_name = `${first_name} ${last_name}`;   
+
+console.log(full_name);
