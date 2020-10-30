09735cb039dd693edf2578a11815b540540e72cc
808kevin <kn42@hawaii.edu>
2020-10-29 14:27:18 -1000

    .

--- Lab13/info_server_Ex1a.js
+++ Lab13/info_server_Ex1a.js
@@ -4,4 +4,5 @@ var app = express();
 app.all('*', function (request, response, next) {
     response.send(request.method + ' to path ' + request.path);
 });
+
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
